c1f22ac9768ef10f300050f2ffdd16b3387473c2
Arsen Niyonzima <arse0012@edu.easj.dk>
2021-05-19 20:08:19 +0200

    ReactExpressNodeMsSql

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
+  const [name, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitAccount = () => {
+    Axios.post("http://localhost:3001/api/addNewData", {
+      name: name, 
+      email: email,
+      password: password,
+    }).then(() => {
+      console.log("successful insert");
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MernSql App</h1>
+
+      <div className="form">
+        <label>User name:</label>
+        <input type="text" 
+        name="name" 
+        onChange={(e) => {
+          setUserName(e.target.value);
+        }} />
+        <label>Email:</label>
+        <input type="text" 
+        name="email" 
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }} />
+        <label>Password:</label>
+        <input type="text" 
+        name="password" 
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }} />
+        <button onClick={submitAccount}>Submit</button>
+      </div>
     </div>
   );
 }
